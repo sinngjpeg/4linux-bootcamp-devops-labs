fb3687b0422c9498c68dda5030627ce8da88436e
unknown <Ingrid Silva>
2022-01-04 23:15:12 -0300

    teste para verificar falha da pipeline

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps Sinng.")
+        self.assertRegex(result.data.decode(), "Pipeline nao DevOps Sinng.")
 
 
 if __name__ == "__main__":
